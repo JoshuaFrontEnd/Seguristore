c469ad8301d6e15bce454d340914f33afdcae557
JTorres27 <hola@joshua.cl>
2017-05-09 20:53:42 -0300

    Menu escritorio terminado, definiendo estilo responsive

---
+++ html/contacto.php
@@ -0,0 +1,55 @@
+<?php
+//validar formulario
+
+	if($_POST){
+
+		$nombre = $_POST["nombre"];
+
+		$email = $_POST["email"];
+
+		$claseNombre = "";
+
+		$claseEmail = "";
+
+		if($nombre == ""){
+
+			$msgNombre = "Ingrese un Nombre";
+
+			$claseNombre = "error";}
+
+		if(!eregi("^([_a-z0-9-]+)(\.[_a-z0-9-]+)*@([a-z0-9-]+)(\.[a-z0-9-]+)*(\.[a-z]{2,4})$",$email)){
+
+			$msgEmail = "Ingrese un Email valido";
+
+			$claseEmail = "error";
+
+			$email = "";}
+
+		if($claseNombre == "" && $claseEmail == ""){
+
+		//funcion mail para enviar los datos del formulario
+		$mensaje="Mensaje nuevo desde el formulario de contacto de www.seguristore.cl";
+
+		$mensaje.= "\n\nNombre: ". $_POST['nombre'];
+
+		$mensaje.= "\n\nEmail: ".$_POST['email'];
+
+		$mensaje.= "\n\nMensaje: \n\n".$_POST['mensaje'];
+
+		$destino= "contacto@seguristore.cl", "hola@joshua.cl";
+
+		$remitente = $_POST['email'];
+
+		$asunto = "Mensaje nuevo desde el formulario de contacto de www.seguristore.cl";
+
+		mail($destino,$asunto,$mensaje,"FROM: $remitente");
+
+		$nombre = "";
+
+		$email = "";
+
+		$mensaje = "Muchas gracias su mensaje ha sido enviado, le responderemos a la brevedad.";
+
+	}
+
+}?>
--- html/js/main.js
+++ html/js/main.js
@@ -1,128 +1,121 @@
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
-
-
-/*-- Scroll puro JS vanilla --*/
-// function scrollIt(destination, duration = 200, easing = 'linear', callback) {
-
-//   const easings = {
-//     linear(t) {
-//       return t;
-//     },
-//     easeInQuad(t) {
-//       return t * t;
-//     },
-//     easeOutQuad(t) {
-//       return t * (2 - t);
-//     },
-//     easeInOutQuad(t) {
-//       return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
-//     },
-//     easeInCubic(t) {
-//       return t * t * t;
-//     },
-//     easeOutCubic(t) {
-//       return (--t) * t * t + 1;
-//     },
-//     easeInOutCubic(t) {
-//       return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
-//     },
-//     easeInQuart(t) {
-//       return t * t * t * t;
-//     },
-//     easeOutQuart(t) {
-//       return 1 - (--t) * t * t * t;
-//     },
-//     easeInOutQuart(t) {
-//       return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t;
-//     },
-//     easeInQuint(t) {
-//       return t * t * t * t * t;
-//     },
-//     easeOutQuint(t) {
-//       return 1 + (--t) * t * t * t * t;
-//     },
-//     easeInOutQuint(t) {
-//       return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t;
-//     }
-//   };
-
-//   const start = window.pageYOffset;
-//   const startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
-
-//   const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
-//   const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
-//   const destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
-//   const destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
-
-//   if ('requestAnimationFrame' in window === false) {
-//     window.scroll(0, destinationOffsetToScroll);
-//     if (callback) {
-//       callback();
-//     }
-//     return;
-//   }
-
-//   function scroll() {
-//     const now = 'now' in window.performance ? performance.now() : new Date().getTime();
-//     const time = Math.min(1, ((now - startTime) / duration));
-//     const timeFunction = easings[easing](time);
-//     window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
-
-//     if (window.pageYOffset === destinationOffsetToScroll) {
-//       if (callback) {
-//         callback();
-//       }
-//       return;
-//     }
-
-//     requestAnimationFrame(scroll);
-//   }
-
-//   scroll();
-// }
-
-// document.querySelector('.js-btn2').addEventListener('click', () => {
-//   scrollIt(
-//     document.querySelector('.js-section2'),
-//     300,
-//     'easeOutQuad',
-//     () => console.log(`Just finished scrolling to ${window.pageYOffset}px`)
-//   );
+// $('<img/>').attr('src', '../img/bgMainBanner2.jpg').on('load', function() {
+//    $(this).remove(); // prevent memory leaks as @benweet suggested
+//    $('.mainBanner').css('background', 'url(../img/bgMainBanner2.jpg) bottom no-repeat');
+//    $('body').css('display', 'block');;
 // });
 
 
+// Dectecto el evento de carga y redimension de la pagina
+$(window).on('load resize', function(){
+
+    var mainBannerH = $('.mainBanner').outerHeight(),
+        mainHeaderH = $('.mainHeader').outerHeight(),
+        mainHeaderNav = $('.mainHeader_nav'),
+        linkNav = mainHeaderNav.find('a[href^="#"]'),
+        anchoVentana = $(window).outerWidth();
+
+    // Tomo la altura del header y se la agrego a los fondos del mismo
+    $('.headerBgHeight').outerHeight(mainHeaderH);
+
+    $(document).on('scroll', onScroll);
+
+    linkNav.on('click', function(e) {
+        e.preventDefault();
+
+        $(document).off('scroll', onScroll);
+        navChangueClickColor(this);
+        animateAnchor(this);
+
+    });
+
+    $(document).on('click scroll', function(e) {
+        e.preventDefault();
+
+        var scrollPos = $(document).scrollTop();
+        replaceColorNav(scrollPos);
+
+    });
+
+    $('.buttonArrow').on('click', function(e) {
+        e.preventDefault();
+
+        animateAnchor(this);
+        mainHeaderNav.find('a').removeClass('activeNav');
+        $('.nosotros').addClass('activeNav');
+
+    });
+
+    function navChangueClickColor($this){
+
+        linkNav.removeClass('activeNav');
+        $($this).addClass('activeNav');
+
+    }
+
+    function animateAnchor($this) {
+
+        var target = $this.hash,
+            menu = target;
+
+        $target = $(target);
+
+        $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+        }, 1000, 'swing', function() {
+            $(document).on('scroll', onScroll);
+        });
+
+    }
+
+    function onScroll(){
+
+        var scrollPos = $(document).scrollTop();
+
+        linkNav.each(function () {
+
+            var currLink = $(this),
+                refElement = $(currLink.attr("href"));
+
+            if ((refElement.position().top - 60) <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+
+                navChangueClickColor(currLink);
+
+            }else{
+
+                currLink.removeClass("activeNav");
+
+            }
+
+        });
+
+    }
+
+    //Funcion que reemplaza el estilo del nav al estar abajo del banner
+    function replaceColorNav(scrollPos){
+
+        if (scrollPos >= (mainBannerH - 60) ){
+            mainHeaderNav.find('a').addClass('replaceColorNav');
+        }else{
+            mainHeaderNav.find('a').removeClass('replaceColorNav');
+        }
+
+    }
+
+    if(anchoVentana >= 768 ){
+        $('.mainHeader').removeClass('active');
+        $('.mainHeader_nav').removeClass('active center');
+
+    }
+
+});
+
+
+$('.btnMostrarMenu').on('click', function(e) {
+e.preventDefault();
+
+    console.log('click');
+
+    $('.mainHeader').toggleClass('active');
+    $('.mainHeader_nav').toggleClass('center active');
+
+});
